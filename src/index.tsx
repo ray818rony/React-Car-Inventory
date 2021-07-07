8195b7df5b46307ea6f63d3367f944b5a3f762d1
Rony Ray Shabo <79472386+ray818rony@users.noreply.github.com>
2021-07-06 23:01:05 -0500

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home, Dashboard, Signin } from './components';
+import './styles.css'
+
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+
+        <Route exact path='/'>
+          <Home title={'Car Inventory || C.I.D'}/>
+        </Route>
+
+        <Route path='/dashboard'>
+          <Dashboard></Dashboard>
+        </Route>
+
+        <Route path='/signin'>
+          <Signin></Signin>
+        </Route>
+
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
